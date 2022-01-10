58c20c5e5666d835bbdf64e1f950a6eb8d12667a
simaoppcastro <simaoppcastro@gmail.com>
2022-01-10 12:12:23 +0000

    minor updates on snake and apple class

--- snake_game.js
+++ snake_game.js
@@ -6,6 +6,83 @@
 const canvas = document.getElementById("canvas")
 const canvasContext = canvas.getContext('2d')
 
+// Snake Class
+class Snake {
+    // snake constructor
+    constructor(x, y, size) {
+        this.x = x
+        this.y = y
+        this.size = size
+        this.tail = [{x:this.x, y:this.y}]
+        this.rotateX = 0
+        this.rotateY = 1
+    }
+
+    // snake move function 
+    move() {
+        let newRect
+
+        if (this.rotateX == 1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x + this.size,
+                y: this.tail[this.tail.length - 1].y
+            }
+        } else if (this.rotateX == -1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x - this.size,
+                y: this.tail[this.tail.length - 1].y
+            }
+        } else if (this.rotateY == 1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x,
+                y: this.tail[this.tail.length - 1].y + this.size
+            }
+        } else if (this.rotateY == -1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x,
+                y: this.tail[this.tail.length - 1].y - this.size
+            }
+        }
+
+        this.tail.shift()
+        this.tail.push(newRect)
+    }
+}
+
+// Apple Class
+class Apple{
+    // apple constructor
+    constructor(){
+        // touching bool flag
+        let isTouching
+        
+        while (true) {
+            isTouching = false;
+            this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size
+            this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size
+            
+            for (let i = 0; i < snake.tail.length; i++) {
+                if (this.x == snake.tail[i].x && this.y == snake.tail[i].y) {
+                    // snake hit/touch the apple
+                    isTouching = true
+                }
+            }
+
+            this.size = snake.size
+            // apple object color reference
+            this.color = "red"
+            
+            if (!isTouching) {
+                break;
+            }
+        }
+    }
+}
+
+// create objects
+const snake = new Snake(x=20,y=20,size=20);
+let apple = new Apple();
+
 window.onload = () => {
     gameLoop()
 }
@@ -94,74 +171,3 @@ window.addEventListener("keydown", (event) => {
     }, 1)
 })
 
-// Snake Class
-class Snake {
-    constructor(x, y, size) {
-        this.x = x
-        this.y = y
-        this.size = size
-        this.tail = [{x:this.x, y:this.y}]
-        this.rotateX = 0
-        this.rotateY = 1
-    }
-
-    move() {
-        let newRect
-
-        if (this.rotateX == 1) {
-            newRect = {
-                x: this.tail[this.tail.length - 1].x + this.size,
-                y: this.tail[this.tail.length - 1].y
-            }
-        } else if (this.rotateX == -1) {
-            newRect = {
-                x: this.tail[this.tail.length - 1].x - this.size,
-                y: this.tail[this.tail.length - 1].y
-            }
-        } else if (this.rotateY == 1) {
-            newRect = {
-                x: this.tail[this.tail.length - 1].x,
-                y: this.tail[this.tail.length - 1].y + this.size
-            }
-        } else if (this.rotateY == -1) {
-            newRect = {
-                x: this.tail[this.tail.length - 1].x,
-                y: this.tail[this.tail.length - 1].y - this.size
-            }
-        }
-
-        this.tail.shift()
-        this.tail.push(newRect)
-    }
-}
-
-// Apple Class
-class Apple{
-    constructor(){
-        let isTouching
-        
-        while (true) {
-            isTouching = false;
-            this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size
-            this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size
-            
-            for (let i = 0; i < snake.tail.length; i++) {
-                if (this.x == snake.tail[i].x && this.y == snake.tail[i].y) {
-                    isTouching = true
-                }
-            }
-
-            this.size = snake.size
-            // apple object color reference
-            this.color = "red"
-            
-            if (!isTouching) {
-                break;
-            }
-        }
-    }
-}
-
-// create objects
-const snake = new Snake(x=20,y=20,size=20);
-let apple = new Apple();
